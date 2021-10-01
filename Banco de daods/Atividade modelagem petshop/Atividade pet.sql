26818e2c321ed76fc206ab048df5cf99d39281b9
Wesleysou <wesley.souza@bandtec.com.br>
2021-10-01 15:58:40 -0300

    Regra de negocio

--- Banco de daods/Atividade modelagem petshop/Atividade pet.sql
+++ Banco de daods/Atividade modelagem petshop/Atividade pet.sql
@@ -25,22 +25,80 @@ idPet int auto_increment primary key,
 nomePet varchar(45),
 raca varchar(45),
 datanascPet date,
+precoPet decimal (10,2),
 fk_cliente int,
 foreign key (fk_cliente) references cliente (idCliente)
 )auto_increment = 101;
 
 insert into Pet values 
-(null,'Craudio','Cahcorro',08/05/2006,3),
-(null,'Dog','Cahcorro',04/03/2003,5),
-(null,'Persa','Gato',30/04/2021,4),
-(null,'Marlei','Peixe',04/12/2017,3),
-(null,'Sino','Peixe',30/12/2012,4),
-(null,'Dragonite','gato',11/03/2011,3),
-(null,'Toto','Cachorro',17/05/2020,6),
-(null,'Toni','Passaro',04/09/2009,2),
-(null,'Thanos','Cachorro',11/13/2010,1),
-(null,'Galho','Passaro'12/12/2010,2);
+(null,'Craudio','Cahcorro',20060506,'28392.32',3),
+(null,'Dog','Cahcorro',20030304,'23221.32',5),
+(null,'Persa','Gato',20210430,'34554.32',4),
+(null,'Marlei','Peixe',20171204,'58392.32',3),
+(null,'Sino','Peixe',20121230,'98392.32',4),
+(null,'Dragonite','gato',20110311,'32492.32',3),
+(null,'Toto','Cachorro',20200517,'654692.32',6),
+(null,'Toni','Passaro',20090904,'2342.32',2),
+(null,'Thanos','Cachorro',2010311,'6548392.32',1),
+(null,'Galho','Passaro',20101210,'100392.32',2);
 
-select * from Pet;
+select * from pet;
 select * from cliente;
+
+drop table pet;
+
+select Cl.Nomecliente, P.nomePet, P.precopet
+from  Pet p
+join Cliente cl on Cl.idCliente = p.fk_cliente
+where  nomePet like 'T%';
+
+-- fazer a soma
+select sum(precopet) from pet;
+-- fazer a media
+select avg(precopet) from pet;
+-- Arredonda o valor
+select round(precopet) from pet;
+-- Mais barato
+select min(precopet) from pet;
+-- Mais caro
+select max(precopet), raca, nomePet from pet;
+-- Contagem 
+select count(raca) from pet;
+-- Define a quantidade de casas decimais
+select truncate(precopet,5) from pet;
+-- exemlo legal 
+select avg(round(precopet,2)) as media_arredondada from pet;
+
+create table agendamento (
+idAgenda int primary key auto_increment,
+nomeagenda varchar(50),
+hora varchar(10),
+dia date,
+fk_cliente int,
+foreign key (fk_cliente) references Cliente(idcliente)
+);
+
+insert into agendamento value
+(null,'Consulta média','10H','20211001',1),
+(null,'Consulta média','9H','20211001',2),
+(null,'Banho','8H','20211001',1),
+(null,'Banho & tosa','7H','20211001',2),
+(null,'Banho','7:30H','20211001',3),
+(null,'Consulta média','8:30H','20211001',3),
+(null,'Cortar unha', '9:30H','20211001',5),
+(null,'Toza','10:30H','20211001',6),
+(null,'Consulta média','11H','20211001',6),
+(null,'Consulta média','12H','20211001',5);
+
+select * from agendamento;
+
+
+select agendamento.*, Cliente.*, Pet.*
+from Cliente 
+join Pet  on Cliente.idCliente = Pet.fk_cliente
+join agendamento on Cliente.idCliente = agendamento.fk_cliente; 
+
+drop table agendamento;
+
+
 
